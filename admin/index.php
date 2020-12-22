ce70978244d861efbedd6dd09468e9e58bdf26c0
SafalFrom2050 <Safal.sharma.ss@gmail.com>
2020-12-23 04:15:28 +0545

    Bugs Fixes

--- admin/index.php
+++ admin/index.php
@@ -11,12 +11,32 @@
 
     include 'header.php';
 ?>
+<br>
+<div class="admin">
+        <div class="left">
+            <a href="#"> TESTING </a>
+        </div>
+        <div class="left">
+            <a href="#"> TESTING </a>
+        </div>
+        <div class="right">
+        <a href="#"> TESTING </a>
+        </div>
 
+    </div>
 <main>
+    <nav>
+		<ul>
+            <li><a href="#">News Articles</a></li>
+            <li><a href="#">Categories</a></li>
+            <li><a href="#">Comments Moderation</a></li>
+		</ul>
+	</nav>
     <article>
     	<h2>Welcome To Admin Section</h2>
 		<p>Options:</p>
-	</article>
+    </article>
+    
 </main>
 
 
